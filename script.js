d63306f7ca484a7f64d9c68b939e5597120ba3d2
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-03-28 14:16:38 +0200

    Debbuged challenge 3

--- script.js
+++ script.js
@@ -4,22 +4,37 @@ const user = 'John';
 //semicolon wasn't included
 
 
-/* This is the lastname of the user */ 
+/** This is the lastname of the user  */
 const surname = 'Smith';
+
 //semicolon wasn't included
 
-console.log('user, surname');
+console.log(user, surname);
 //semicolon was inside brackets 
 //and there wasn't any ouside the bracket
 
 /* (c) ACME Inc. 2010 */
 
-/*
- * This is the date that a user created their account 
- */
+/** This is the date that a user created their account*/ 
+
 const date = '10/07/2014';
 
-console.log('date');
+console.log(date);
+
+/*
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn('Security scan starting');
+
+/* It is important to let user know when they can close the page*/ 
+
+console.info('Please wait for scan to complete before closing the browser.');
+
+
 
 
 
