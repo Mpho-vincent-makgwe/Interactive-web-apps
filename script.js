45663c6603a450f3f2f4699df5ecf982ac6eff4e
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-03-28 13:55:02 +0200

    Debbuged challenge 3

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 /* This is the firstname of the user */
 
 const user = 'John';
+//semicolon wasn't included
 
 
 /* This is the lastname of the user */ 
 const surname = 'Smith';
+//semicolon wasn't included
 
 console.log('user, surname');
+//semicolon was inside brackets 
+//and there wasn't any ouside the bracket
+
+/* (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014';
+
+console.log('date');
 
 
 
