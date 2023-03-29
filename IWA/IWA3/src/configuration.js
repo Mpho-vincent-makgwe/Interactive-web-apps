bb8c674f63bdcae646cdbdb08bf4896ea8b065be
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-03-29 08:32:45 +0200

    ADDED NEW FOLDERS AND STARTED WITH MY CHALLENGE 3

--- IWA/IWA3/src/configuration.js
+++ IWA/IWA3/src/configuration.js
@@ -1,4 +1,4 @@
 // configuration.js
 
-const company = 'ACME Inc.';
-const year = 2022;
+export const company = 'ACME Inc.';
+export const year = 2022;
--- IWA/IWA3/src/scripts.js
+++ IWA/IWA3/src/scripts.js
@@ -1,7 +1,11 @@
 // scripts.js
+// the imports in the script file are incorrect. They should be:
+import {company} from './configurations.js';//and full stops ans forward slashes had to be included.
 
-import company form 'configuration'
-import year form 'configuration'
+// the variable names in the import statements are misspelled. It should be:
+import {year} from './configurations.js';//and full stops ans forward slashes had to be included.
 
-const message = '© ' + company + ' (' + year + ')'
-document.querySelector('footer').innerText = message
+const message = '© ' + company + ' (' + year + ')';
+document.querySelector('[data-key="footer"]').innerText = message;
+
+console.log(message);
