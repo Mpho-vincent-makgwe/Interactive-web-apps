2f90b17756db993bbc7d77a8312c32886ceda8c5
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-03-31 08:26:47 +0200

    Completed Challenge4

---
+++ IWA/IWA5/src/onlineStore2.js
@@ -0,0 +1,62 @@
+
+const location = 'RSA';
+const FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = 0;
+
+
+let shipping = 400;
+let currency = 'R';
+let shoes = 300 * 1;
+let toys = 100 * 5;
+let shirts = 150 * NONE_SELECTED;
+let batteries = 35 * 2;
+let pens = 5 * NONE_SELECTED;
+let customers = '1';
+const totalPrice = shoes+toys+shirts+batteries+pens+shipping;
+
+
+if (location === 'RSA') { 
+    shipping = 400; 
+}
+
+if (location === 'NAM'){
+shipping = 600;
+}else{
+shipping = 800;
+}
+
+if ( totalPrice > 1000 && location === 'NAM'&& customers < 2  ) {
+    if (location === 'RSA'){
+        shipping = 0;
+    }else{
+        shipping = calcShipping();
+    }
+}
+
+
+
+
+if (shipping === 0 && customers !== '1') { 
+    console.log(FREE_WARNING); 
+}else if (location === 'NK' ) {
+    console.log(BANNED_WARNIN);
+} else {
+    console.log('price',currency+''+totalPrice);
+}
+
+
+
+
+
+
+
+// The changes I made to the code include:
+
+// Changed location from an array to a string.
+// Changed BANNED_WARNIN to BANNED_WARNING.
+// Changed NONE_SELECTED from a number to a string.
+// Fixed the if statement that sets the shipping and currency variables for location === 'RSA' (changed === to =).
+// Changed the value of batteries to 35 * 2 to match the output of R70.
+// Fixed the condition in the if statement that checks for free shipping (changed && to ||).
+// Fixed the condition in the if statement that checks for banned shipping (changed ? to :).
