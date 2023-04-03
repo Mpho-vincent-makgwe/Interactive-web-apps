e539d8bea342884a5574414c9d54a4e9bc7572d0
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-04-03 13:37:28 +0200

    Debbuged my both challenges

---
+++ IWA/IWA7/src/Challenge2/Challenge2.js
@@ -0,0 +1,6 @@
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+console.log(`Good Morning, ${nickname} || ${firstname}!`);/* I used backticks to enclose the string and used ${} on both of the variables to interpolate them.*/
+//Removed double quotes
+
---
+++ IWA/IWA7/src/challenge1/Challenge1.js
@@ -0,0 +1,2 @@
+const value = parseInt("3");/*used parseInt to convert 3 from a string to a decimal number.Or to treat 3 as an actgual addeble number*/
+console.log(value + 4 + value);
