a4ba0ff2857bca8c63cb2641f18fa7649d16cc11
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-04-11 13:45:50 +0200

    completed Challenge 1 Ready to submit IWA 9

--- IWA/IWA9/Challenge1.js
+++ IWA/IWA9/Challenge1.js
@@ -5,33 +5,34 @@ const size = 'large';
 
 // Only change the syntax below (not the values or key names)
 
+
 const expenses = {
     food: 51.7501,
     transport:  10.2,
-}
-  
+};
+
 const tax = {
     734: '3%',
     234: '20%',
     913: '12%',
     415: '38%',
     502: '42%',
-}
+};
 
 const rent = {
     none: 0,
-    smallRoom : 200,
-    largeRoom: 300,
-    smallApartment: 400,
-    largeApartment: 800,
-    smallFouse: 1200,
-    largeHouse: 2400,
-}
+    'small-room': 200,
+    'large-room': 300,
+    'small-apartment': 400,
+    'large-apartment': 800,
+    'small-house': 1200,
+    'large-house': 2400,
+};
 
 // You can change below however you want
-
-const taxAsDecimal = parseFloat(tax*0.913) / 100;
-const startingAfterTax = salary * 1 - taxAsDecimal
-const type = lodging + size
-const balance = expenses.transport - expenses.food - expenses(rent.type) 
-console.log(balance)
+const Rand = 'R';
+const taxAsDecimal = parseFloat(tax[913]) / 100;
+const startingAfterTax = salary * (1 - taxAsDecimal);
+const type = `${lodging} + '-' + ${size}`;
+const balance =  startingAfterTax - (expenses.transport + expenses.food + rent["large-apartment"]);
+console.log(Rand,balance.toFixed(2));
