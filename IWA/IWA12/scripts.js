bc77738662e9946db0532188f7732d3a11ec67c6
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-04-14 15:45:26 +0200

    Still debbuging

--- IWA/IWA12/scripts.js
+++ IWA/IWA12/scripts.js
@@ -44,19 +44,19 @@ const reserve3 = document.querySelector('div#book3 button.reserve');
 const checkout3 = document.querySelector('div#book3 button.checkout');
 const checkin3 = document.querySelector('div#book3 button.checkin');
 
-checkin1.style.color = 'none';
+checkin1.style.color = '';
 status1.style.color = STATUS_MAP[status1.innerText].color;
 reserve1.disabled = !STATUS_MAP[status1.innerText].canReserve;
 checkout1.disabled = !STATUS_MAP[status1.innerText].canCheckout;
 checkin1.disabled = !STATUS_MAP[status1.innerText].canCheckIn;
 
-checkin2.style.color = 'none';
+checkin2.style.color = '';
 status2.style.color = STATUS_MAP[status2.innerText].color;
 reserve2.disabled = !STATUS_MAP[status2.innerText].canReserve;
 checkout2.disabled = !STATUS_MAP[status2.innerText].canCheckout;
 checkin2.disabled = !STATUS_MAP[status2.innerText].canCheckIn;
 
-checkin3.style.color = 'none';
+checkin3.style.color = '';
 status3.style.color = STATUS_MAP[status3.innerText].color;
 reserve3.disabled = !STATUS_MAP[status3.innerText].canReserve;
 checkout3.disabled = !STATUS_MAP[status3.innerText].canCheckout;
