898c11cce477eb47721654da445377675e53f4b9
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-06-04 19:51:45 +0200

    installed ESlint

--- src/IWA/IWA11/script.js
+++ src/IWA/IWA11/script.js
@@ -2,7 +2,7 @@
 
 const order1 = document.querySelector('[data-key="order1"]');
 
-/* const 1-root = document(order1),*/
+/* const 1-root = document(order1), */
 const biscuits1 = order1.querySelector('.biscuits .count');
 const donuts1 = order1.querySelector('.donuts .count');
 const pancakes1 = order1.querySelector('.pancakes .count');
@@ -11,14 +11,10 @@ const status1 = order1.querySelector('.status dd');
 biscuits1.textContent = order1.getAttribute('data-biscuits');
 donuts1.textContent = order1.getAttribute('data-donuts');
 pancakes1.textContent = order1.getAttribute('data-pancakes');
-status1.textContent = order1.getAttribute('data-delivered') !== 'false'  ? 'Delivered' : 'Pending';
-
-
-
-
+status1.textContent = order1.getAttribute('data-delivered') !== 'false' ? 'Delivered' : 'Pending';
 
 const order2 = document.querySelector('[data-key="order2"]');
-/* const 2-root = document(order2),*/
+/* const 2-root = document(order2), */
 const biscuits2 = order2.querySelector('.biscuits .count');
 const donuts2 = order2.querySelector('.donuts .count');
 const pancakes2 = order2.querySelector('.pancakes .count');
@@ -31,7 +27,7 @@ status2.textContent = order2.getAttribute('data-delivered') !== 'false' ? 'Deliv
 
 const order3 = document.querySelector('[data-key="order3"]')
 
-/* const 3-root = document(order3),*/
+/* const 3-root = document(order3), */
 const biscuits3 = order3.querySelector('.biscuits .count');
 const donuts3 = order3.querySelector('.donuts .count');
 const pancakes3 = order3.querySelector('.pancakes .count');
@@ -41,4 +37,3 @@ biscuits3.textContent = order3.getAttribute('data-biscuits');
 donuts3.textContent = order3.getAttribute('data-donuts');
 pancakes3.textContent = order3.getAttribute('data-pancakes');
 status3.textContent = order3.getAttribute('data-delivered') !== 'false' ? 'Delivered' : 'Pending';
-
